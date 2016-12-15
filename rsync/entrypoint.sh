694149f9b88fa3fb590f523ed6072625bcc7590f
will Durand <will+git@drnd.me>
2016-12-15 23:00:06 +0100

    Fix ssh-add

--- rsync/entrypoint.sh
+++ rsync/entrypoint.sh
@@ -5,7 +5,7 @@ eval $(ssh-agent -s)
 
 # add ssh key stored in SSH_PRIVATE_KEY variable to the agent store
 echo "$SSH_PRIVATE_KEY" > ssh_private_key
-ssh-add < ssh_private_key
+ssh-add ssh_private_key
 rm ssh_private_key
 
 # disable host key checking (NOTE: makes you susceptible to man-in-the-middle attacks)
