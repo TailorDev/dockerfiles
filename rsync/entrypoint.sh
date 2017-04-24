a8850707fcb6844ccf37a4770d8777c44c46bd17
William Durand <will+git@drnd.me>
2017-04-24 16:18:29 +0200

    Allow to specify a passphrase in tailordev/rsync

--- rsync/entrypoint.sh
+++ rsync/entrypoint.sh
@@ -3,11 +3,20 @@
 # run ssh-agent
 eval $(ssh-agent -s)
 
+# create askpass script
+SSH_ASKPASS_SCRIPT=/tmp/ssh-askpass-script
+cat > "$SSH_ASKPASS_SCRIPT" <<EOL
+#!/bin/bash
+echo cat
+EOL
+chmod +x "$SSH_ASKPASS_SCRIPT"
+
 # add ssh key stored in SSH_PRIVATE_KEY variable to the agent store
-echo "$SSH_PRIVATE_KEY" > ssh_private_key
-chmod 600 ssh_private_key
-ssh-add ssh_private_key
-rm ssh_private_key
+SSH_PRIVATE_FILE=/tmp/ssh_private_key
+echo "$SSH_PRIVATE_KEY" > "$SSH_PRIVATE_FILE"
+chmod 600 "$SSH_PRIVATE_FILE"
+echo "$SSH_PASSPHRASE"| SSH_ASKPASS="$SSH_ASKPASS_SCRIPT" ssh-add "$SSH_PRIVATE_FILE"
+rm "$SSH_PRIVATE_FILE" "$SSH_ASKPASS_SCRIPT"
 
 # disable host key checking (NOTE: makes you susceptible to man-in-the-middle attacks)
 mkdir -p ~/.ssh
